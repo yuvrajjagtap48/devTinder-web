d92271f6c733e53cb1bc68fdef1d0b4429215fa6
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-12-01 10:04:38 +0530

    Routing

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={ <Body /> } >
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
