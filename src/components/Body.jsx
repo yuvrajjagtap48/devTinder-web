47116179219046a74e4875f0ecd0de79c1055c6d
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-12-20 19:47:09 +0530

    redirect to login if not auth

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBar'
-import Footer from './Footer'
+import { Outlet , useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+
+
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+    const fetchUser = async () => {
+      if (userData) return; 
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data))
+    } catch (err) {
+      if ( err.status === 401){
+      navigate("/login");
+      }
+      console.log(err);
+    }
+  }; 
+
+  useEffect(() => {
+    fetchUser();
+  },[]);
+
   return (
     <div>
-     <NavBar />
-     <Outlet />
-     <Footer />
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 const user = useSelector ((store)=>store.user);
@@ -8,7 +9,7 @@ console.log("User in NavBar:", user);
   return (
      <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl"> 🧑‍💻 DevTinder</a>
+    <Link to="/" className="btn btn-ghost text-xl"> 🧑‍💻 DevTinder</Link>
   </div>
   <div className="flex gap-2">
       <div className="form-control"></div>
@@ -27,10 +28,10 @@ console.log("User in NavBar:", user);
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
